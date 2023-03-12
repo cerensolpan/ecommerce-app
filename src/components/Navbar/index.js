4a3197de062fe5aa1eb5bde5d6733afa94aa4aaa
cerensolpan <cerensolpan@gmail.com>
2023-03-12 03:24:25 +0300

    Add search input and basket button to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,105 @@
 import "./style.scss";
-import logo from "../../assets/icons/logo.svg";
-import basket from "../../assets/icons/basket.svg";
-import Button from "../Ui/Button";
+import { useState, useEffect } from "react";
+import { Button } from "../ui/Button";
+import Icon from "../Icon";
+import { SearchInput } from "../ui/SearchInput";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const basket = useSelector((state) => state.basket);
+
+  const [searchText, setSearchText] = useState("");
+  const [percentage, setPercentage] = useState(0);
+
+  const searchProduct = (e) => {
+    setSearchText(e.target.value);
+  };
+  const pressEnter = (e) => {
+    if (e.key === "Enter") {
+      changeNavigate();
+    }
+  };
+
+  const changeNavigate = () => {
+    if (searchText.length >= 3) {
+      navigate({
+        pathname: "/",
+        search: `?search=${searchText}`,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const percentageBasket = (basket.totalPrice / 500) * 100;
+    setPercentage(percentageBasket);
+  }, [basket]);
+
   return (
-    <div className="navbar">
-      <img src={logo} width={163} height={31} alt="logo" />
-      <Button size="sm" color="primary">
-        <img src={basket} width={23} height={23} alt="basket" />
-        Sepetim
-      </Button>
-    </div>
+    <>
+      <div className="container">
+        <div className="navbar">
+          <a href="/">
+            <Icon iconName="logo" />
+          </a>
+          <div className="navbar-search">
+            <SearchInput
+              icon
+              placeholder="Ürün ara..."
+              onChange={searchProduct}
+              onClick={changeNavigate}
+              onKeyDown={pressEnter}
+            />
+          </div>
+          <div className="header-basket-wrapper">
+            <Button text="Sepetim" icon="basket" />
+            <span className="header-basket-basketQuantity">
+              {basket.totalQuantity}
+            </span>
+            {basket.totalQuantity > 0 && (
+              <div className="header-basket-shipping">
+                <div className="header-basket-shipping-desc">
+                  <Icon
+                    iconName="attention"
+                    className="header-basket-shipping-icon"
+                  />
+                  {basket.totalPrice - 500 < 0 ? (
+                    <span>
+                      <span className="basket-text">
+                        {Math.ceil(500 - basket.totalPrice)} TL
+                      </span>{" "}
+                      ürün daha ekleyin kargo bedava
+                    </span>
+                  ) : (
+                    <span>Kargonuz bedava</span>
+                  )}
+                </div>
+                <div
+                  className="header-basket-shipping-bar"
+                  style={{ "--bar-width": `${percentage}%` }}
+                ></div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div
+          className={`navbar-responsive-search ${
+            basket.totalQuantity > 0 && `-cart-responsive-active`
+          }`}
+        >
+          <SearchInput
+            icon
+            placeholder="Ürün ara..."
+            onChange={searchProduct}
+            onClick={changeNavigate}
+            onKeyDown={pressEnter}
+          />
+        </div>
+      </div>
+      <div className="header">
+        <h1 className="container">ÇiçekSepeti</h1>
+      </div>
+    </>
   );
 }
